da5aecc226eb14f752f25c967c2e710d97e9b34d
mishal <mishaal79@gmail.com>
2020-12-09 00:12:09 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ansible automation of dev workstation
+
+Steps to setup dev workstation
+
+Current Distribution : Ubuntu
+
+- run bashscript on dev workstation
+- bashscript installs Ansible
+- run playbook yml file
+- installs software tools, setup dev env
+
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install Ansible on Ubuntu
+if [ -n "$( uname -a | grep -i ubuntu)" ]; then
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+    # Pull Ansible playbook and run Install
+    sudo ansible-pull -U https://github.com/<mishaal79>/autodevenv.git
+fi
