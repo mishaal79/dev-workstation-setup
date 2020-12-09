a8967b3b9360bf9e217178d1f4b1d5fc610e64ee
mishal <mishaal79@gmail.com>
2020-12-09 12:37:13 +1100

    Update repostiory for ansible pull

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ if [ -n "$( uname -a | grep -i ubuntu)" ]; then
     # sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt install -y ansible
     # Pull Ansible playbook and run Install
-    sudo ansible-pull -U https://github.com/<mishaal79>/autodevenv.git
+    sudo ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git
 fi
