ae791eb3108adbfcd945a453dba46cd2e48792c4
mishal <mishaal79@gmail.com>
2020-12-09 12:23:53 +1100

    Add yes option for apt install

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if [ -n "$( uname -a | grep -i ubuntu)" ]; then
     sudo apt update
     # sudo apt install software-properties-common
     # sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
+    sudo apt install -y ansible
     # Pull Ansible playbook and run Install
     sudo ansible-pull -U https://github.com/<mishaal79>/autodevenv.git
 fi
