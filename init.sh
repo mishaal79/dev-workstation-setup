477387dc20bb5429e7f7ca62b48238bbfd4f01e1
mishal <mishaal79@gmail.com>
2021-01-26 14:01:48 +1100

    add git to bash file.

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if [ -n "$( uname -a | grep -i ubuntu)" ]; then
     sudo apt update
     # sudo apt install software-properties-common
     # sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install -y ansible
+    sudo apt install -y ansible git
     # Pull Ansible playbook and run Install
     sudo ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git
 fi
