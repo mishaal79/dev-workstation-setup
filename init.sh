7ee25bdd74c359da79680284458da5a2c63c2e04
mishal <mishaal79@gmail.com>
2021-01-28 12:43:13 +1100

    add more packages.

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -xe
 
+# SCRIPT SHOULD BE RUNAS ROOT
+
 # Install Ansible on Ubuntu
 if [ -n "$( uname -a | grep -i ubuntu)" ]; then
-    sudo apt update
-    # sudo apt install software-properties-common
-    # sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install -y ansible git
+     apt update
+    #  apt install software-properties-common
+    #  apt-add-repository --yes --update ppa:ansible/ansible
+     apt install -y ansible git
     # Pull Ansible playbook and run Install
-    sudo ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git
+     ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git
 fi
