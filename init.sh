c9640ee095225bd035ad7d324e1efcef1f5bf4dd
mishal <mishaal79@gmail.com>
2021-01-28 14:21:41 +1100

    Install latest ansible.

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@
 # Install Ansible on Ubuntu
 if [ -n "$( uname -a | grep -i ubuntu)" ]; then
      apt update
-    #  apt install software-properties-common
-    #  apt-add-repository --yes --update ppa:ansible/ansible
+     apt install software-properties-common
+     apt-add-repository --yes --update ppa:ansible/ansible
      apt install -y ansible git
     # Pull Ansible playbook and run Install
-     ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git
+     ansible-pull -U https://github.com/mishaal79/dev-workstation-setup.git -i localhost playbook.yml
 fi
